b1696c4c23c72d253e5ab53f1ff9432ec9394b42
Jarek Páral <paral.jarek@gmail.com>
2017-03-30 01:04:06 +0200

    init fw

---
+++ fw/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * fw.cpp
+ * 3piRB-shield fw
+ * Created: 30.03.2017 0:58:50
+ * Author : JarekParal
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
